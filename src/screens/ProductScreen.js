4695cccc5f8f170e37da95e8a7f195dfc0f89c2a
shaked777 <shaked.g1010@gmail.com>
2023-02-21 00:17:35 +0200

    adding redux

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,64 +1,91 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import products from '../products'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listProductDetails } from "../actions/productActions";
 
-function ProductScreen({ match }) {
+function ProductScreen() {
+  const dispatch = useDispatch();
   const { id } = useParams();
-  const product = products.find((p) => String(p._id) === id);
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(listProductDetails(id));
+  }, [dispatch, id]);
+
   return (
     <div>
-      <Link to='/' className='btn btn-light my-3'>Continue Shopping</Link>
-      <Row>
-        <Col md={6}>
-        <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-          <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>            
-            <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} Reviews`} color={'#f8e825'} />
-            </ListGroup.Item>            
-            <ListGroup.Item>
-              <strong>Price: ${product.price}</strong>
-            </ListGroup.Item>            
-            <ListGroup.Item>
-              {product.description}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      <Link to="/" className="btn btn-light my-3">
+        Continue Shopping
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
             <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>price:</Col>
-                  <Col>
-                  <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
-
               <ListGroup.Item>
-                <Row>
-                  <Col>status:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? "In stock" : "Out of stock"}
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} Reviews`}
+                  color={"#f8e825"}
+                />
               </ListGroup.Item>
-              <ListGroup.Item className='d-grid gap-2'>
-                <Button className='btn-block' size="lg" disabled={product.countInStock == 0} type='button'>Add to cart</Button>
+              <ListGroup.Item>
+                <strong>Price: ${product.price}</strong>
               </ListGroup.Item>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In stock" : "Out of stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-grid gap-2">
+                  <Button
+                    className="btn-block"
+                    size="lg"
+                    disabled={product.countInStock == 0}
+                    type="button"
+                  >
+                    Add to cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
-  )
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
